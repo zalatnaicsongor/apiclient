0525d7c84910d0f334cbed9f19b0cebb0863f857
Csongor Zalatnai <csongor@zalatnai.hu>
2016-07-24 23:30:46 +0100

    initialized spring boot application

---
+++ src/main/java/hu/zalatnai/FHRSAPIConfiguration.java
@@ -0,0 +1,32 @@
+package hu.zalatnai;
+
+import javax.validation.constraints.NotNull;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ConfigurationProperties(prefix = "fhrsapi")
+public class FHRSAPIConfiguration {
+    @NotNull
+    private String authoritiesBasicUri;
+
+    @NotNull
+    private String establishmentsUri;
+
+    public String getAuthoritiesBasicUri() {
+        return authoritiesBasicUri;
+    }
+
+    public void setAuthoritiesBasicUri(String authoritiesBasicUri) {
+        this.authoritiesBasicUri = authoritiesBasicUri;
+    }
+
+    public String getEstablishmentsUri() {
+        return establishmentsUri;
+    }
+
+    public void setEstablishmentsUri(String establishmentsUri) {
+        this.establishmentsUri = establishmentsUri;
+    }
+}
---
+++ src/main/java/hu/zalatnai/FoodHygieneRatingsApplication.java
@@ -0,0 +1,11 @@
+package hu.zalatnai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodHygieneRatingsApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(FoodHygieneRatingsApplication.class, args);
+	}
+}
