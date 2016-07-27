09d831738bd4c15c4534b49b4a735d14fe0ead6c
Csongor Zalatnai <csongor@zalatnai.hu>
2016-07-27 02:04:55 +0100

    enabled CORS

--- src/main/java/hu/zalatnai/foodhygieneratings/FHRSAPIConfiguration.java
+++ src/main/java/hu/zalatnai/foodhygieneratings/FHRSAPIConfiguration.java
@@ -6,6 +6,9 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
 @ConfigurationProperties(prefix = "fhrsapi")
@@ -27,5 +30,15 @@ public class FHRSAPIConfiguration {
     @Bean
     public RestTemplate restTemplate() {
         return new RestTemplate();
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**");
+            }
+        };
     }
 }
